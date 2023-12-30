916a962b68467f367c7449435e6a3745046d41d4
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-31 01:06:23 +0530

    changing

---
+++ robo-shop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-0a3b85d108db5d5f9
+INSTANCES=("mongodb" "redis" "rabbit mq" "catalogue" "user" "cart"
+"mysql" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+  echo "instance: $INSTANCE"
+
+  if [$i == "mongodb"] || [$i == "mysql"] || [$i == "shipping"]
+
+  then
+  INSTANCE_TYPE="t3.small"
+  else
+  INSTANCE_TYPE="t2.micro"
+  fi
+
+
+ aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE 
+--security-group-ids sg-0a3b85d108db5d5f9
+done
