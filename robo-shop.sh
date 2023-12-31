8fc07ef5304f7ab4d20e1603df2f2647bf2e83d9
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-31 11:35:24 +0530

    changed

--- robo-shop.sh
+++ robo-shop.sh
@@ -8,9 +8,7 @@ INSTANCES=("mongodb" "redis" "rabbit mq" "catalogue" "user" "cart"
 for i in "${INSTANCES[@]}"
 do
   echo "instance is: $i"
-
-  if [$i == "mongodb"] || [$i == "mysql"] || [$i == "shipping"]
-
+  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
   then
   INSTANCE_TYPE="t3.small"
   else
@@ -18,6 +16,5 @@ do
   fi
 
 
- aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE 
---security-group-ids sg-0a3b85d108db5d5f9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+ aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0a3b85d108db5d5f9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
